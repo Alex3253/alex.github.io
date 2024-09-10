1826e2c8f4f1d700ed8876e76c59355fd5e947f1
Alex3253 <152949338+Alex3253@users.noreply.github.com>
2024-09-10 14:41:53 -0500

    Delete TClase04_02_09_2024 directory

--- TClase04_02_09_2024/js/app02.js
+++
@@ -1,8 +0,0 @@
-//Mostrar información mediante una ventana emergente
-alert("Instituto CERTUS");
-
-//Mostrar información en el documento de HTML
-document.write("Curso de Backend");
-
-//Mostrar información en la consola
-console.log("Aplicación de NodeJS");
