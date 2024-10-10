9b34954f378e5739896dc30b18d6c928751dde4d
Alex3253 <152949338+Alex3253@users.noreply.github.com>
2024-10-10 16:26:56 -0500

    Delete JClase19S7_9-10-24 directory

--- JClase19S7_9-10-24/ProyectoS07Cl_19/src/POO_Laboratorio/Principal.java
+++
@@ -1,8 +0,0 @@
-package POO_Laboratorio;
-/*
-Diseñe la clase alumno en el paquet POO_laboratorio con os atributos
-codigo(int)nombre(String)
-*/
-public class Principal {
-    //Instanciar un objeto de la clase alumno
-}
